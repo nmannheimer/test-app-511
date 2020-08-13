f40f41aef1044aca50702f53ec0c5d6a72e89130
nmannheimer <nathanmannheimer@gmail.com>
2020-08-12 19:13:56 -0700

    Update app.py

--- app.py
+++ app.py
@@ -10,31 +10,44 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+cars = pd.read_csv("original cars.csv", sep=";")
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+colors = {'US':1, 'Europe':2, 'Japan':3}
+cars['Origin_col'] = [colors[i] for i in cars['Origin']]
+
+fig = px.bar(cars, x='Origin', y='MPG', color='Origin')
+
+fig2 = px.scatter(cars, x="MPG", y="Weight", color="Origin")
+
+fig3 = px.parallel_coordinates(cars,
+                              color='Origin_col',
+                              color_continuous_scale=px.colors.diverging.Tealrose,
+                              color_continuous_midpoint=2)
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='Automobile Exploration'),
 
     html.Div(children='''
-        Dash: A web application framework for Python.
+        Understand Key Performance Trends in the Cars Dataset
     '''),
 
     dcc.Graph(
         id='example-graph',
         figure=fig
+    ),
+    dcc.Graph(
+        id='example-graph2',
+        figure=fig2
+    ),
+    dcc.Graph(
+        id='example-graph3',
+        figure=fig3
     )
 ])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
 
 
 
